1394debd9620bb27ac6fc7621b588bf32d71c824
T.K <nique@niques-air.myfiosgateway.com>
2022-07-30 11:23:43 -0400

    editing information

--- models/transaction.js
+++ models/transaction.js
@@ -1,21 +1,21 @@
 module.exports = [
     {
         item_name: "Neo Tarot",
-        amount: -23,
+        amount: -23.00,
         date: "July 20, 2022",
         from: "Amazon",
         category: "lifestyle",
     },
     {
         item_name: "Direct Deposit",
-        amount: 3000,
+        amount: 3000.00,
         date: "July 22, 2022",
         from: "Employer",
         category: "Income",
     },
     {
         item_name: "Transfer",
-        amount: 600,
+        amount: 600.00,
         date: "July 25, 2022",
         from: "Bank",
         category: "Savings",
